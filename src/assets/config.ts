4c14e5cfe485dbc4901eebc7bfe5de3d2f67d23a
Stefanus <stefbuys21@gmail.com>
2018-08-02 11:51:43 +0200

    untrack

---
+++ src/assets/config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  fire: {
+    apiKey: "AIzaSyB81P8mwQ0vEX0epdQSQam7bcajW0Zo918",
+    authDomain: "rugbot-38c57.firebaseapp.com",
+    databaseURL: "https://rugbot-38c57.firebaseio.com",
+    projectId: "rugbot-38c57",
+    storageBucket: "rugbot-38c57.appspot.com",
+    messagingSenderId: "1049949841310"
+  }
+}
